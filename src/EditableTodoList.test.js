aa1364d676d2ddb5e91e155a2b29193500abb8ff
robin <robinnsheen@berkeley.edu>
2022-07-15 14:48:57 -0700

    editabletodolist test

--- src/EditableTodoList.test.js
+++ src/EditableTodoList.test.js
@@ -4,32 +4,32 @@ import EditableTodoList from "./EditableTodoList";
 
 let testTodos = [
   {
+    id: 1,
     title: "testTitle1",
     priority: 1,
     description: "test description"
   },
-  {
+  { id: 2,
     title: "testTitle2",
     priority: 2,
     description: "test description"
   },
-  {
-    title: "testTitle2",
+  { id: 3,
+    title: "testTitle3",
     priority: 3,
     description: "test description"
   }
 ];
 
+const mockUpdate = jest.fn();
+const mockRemove = jest.fn();
+
 describe("EditableTodosList", function () {
   it("renders without crashing", function () {
-    const mockUpdate = jest.fn();
-    const mockRemove = jest.fn();
     render(<EditableTodoList todos={testTodos} update={mockUpdate} remove={mockRemove} />);
   });
 
   it("matches snapshot", function () {
-    const mockUpdate = jest.fn();
-    const mockRemove = jest.fn();
     const { container } = render(
       <EditableTodoList
         todos={testTodos}
@@ -37,6 +37,21 @@ describe("EditableTodosList", function () {
         remove={mockRemove} />);
     expect(container).toMatchSnapshot();
   });
+
+  it("renders all editable todos in todos", function() {
+    const result = render(
+      <EditableTodoList
+        todos={testTodos}
+        update={mockUpdate}
+        remove={mockRemove} />);
+
+    result.debug(result.container);
+
+    expect(result.getByText("testTitle1")).toBeInTheDocument();
+    expect(result.getByText("testTitle2")).toBeInTheDocument();
+    expect(result.getByText("testTitle3")).toBeInTheDocument();
+
+  })
 
 
 });
